113a0604e5fa45d7bb431b454b4f3f1eaa9a70d2
emerson <emerson.soares.sc@gmail.com>
2022-03-26 21:29:53 -0300

    COMMIT1

--- app.py
+++ app.py
@@ -1,27 +1,68 @@
 import streamlit as st 
+import streamlit_authenticator as stauth
 import pandas as pd 
-from functions.fluxodecaixa import importaContas
-
-st.sidebar.write('Shopfarma Analyser')
-option = st.sidebar.selectbox('Selecione uma opção',('Fluxo de Caixa', 'Calculadora de datas'))
-if option == 'Fluxo de Caixa':
-    periodo = st.sidebar.selectbox('Selecione o período a ser analisado:', ('Mensal', 'Trimestral', 'Anual'))
-    if periodo == 'Mensal':
-        ano = st.sidebar.selectbox('Escolha o ano correspondente', ('2021','2022','2023'))
-        mes = st.sidebar.selectbox('Escolha o mês correspondente', ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'))
-        if (mes == 'Janeiro') or (mes == 'Março') or (mes == 'Maio') or (mes == 'Julho') or (mes == 'Agosto') or (mes == 'Outubro') or (mes == 'Dezembro'):
-            diasMes = 31
-        elif (mes == 'Fevereiro'):
-            diasMes = 28
-        else:
-            diasMes = 30
-
-dfMes = pd.DataFrame()
-for i in range(1,diasMes):
-    dfMes[i] = i
+from functions.ImportaRelatorios import VendasPorDatas
 
 
+st.sidebar.image('images\logo_shopfarama.png')
+names = ['Emerson Soares']
+usernames = ['emerson']
+passwords = ['123']
+hashed_passwords = stauth.Hasher(passwords).generate()
+authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
+    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+name, authentication_status, username = authenticator.login('Login','sidebar')
 
-df = importaContas()
+if authentication_status: 
+    
+    # Menu Lateral
+    st.sidebar.write('Bem-vindo *%s*' % (name)) 
+    with st.sidebar.expander('Análise de dados'):
+        st.write('Evolução do número de clientes')
+        st.write('Evolução do Ticket Médio')
+        st.write('Análise de filial')
+    with st.sidebar.expander('Análise de dados'):
+        st.write('Evolução do número de clientes')
+        st.write('Evolução do Ticket Médio')
+        st.write('Análise de filial')
+    
+    #################################################################
+    # Dashboard Principal
+    #################################################################
 
-st.write(dfMes)
+    # Gráfico com evolução das vendas mês a mês selecionável por loja
+    # Gráfico com evolução do numero de clientes mês a mês
+    # Gráfico de ticket médio de loja e de funcionários
+    # Gráfico de Laboratórios mais vendidos
+    # Gráfico de distribuidoras mais compradas
+    # Gráfico com participação do tipo de custos 
+    # Gráfico de saúde financeira das lojas
+
+    # Filtrar dados por loja
+    loja = st.selectbox('Selecione uma loja:',('CENTRAL', 'MATRIZ', 'RIO VERMELHO', 'VARGEM', 'CENTRINHO', 'CALIL'))
+    metricas = {
+        'venda_liquida':10
+    }
+    # Lineup --- Venda Líquida - Entrada de Capital - Saída de Capital - Saldo de Capital
+    met1, met2, met3, met4 = st.columns(4)
+    met1.metric(label='Venda Líquida', value=metricas['venda_liquida'], delta='-0.6')
+    met2.metric(label='Entrada Efetiva de Capital', value='92.4K', delta='-0.8')
+    met3.metric(label='Saída Efetiva de Capital', value='-93.0K', delta='10.6')
+    met4.metric(label='Saldo Efetivo de Capital', value='-0.6K', delta='-0.1')
+
+    # Gráfico com evolução das vendas mês a mês selecionável por loja
+    with st.expander('Evolução das vendas mês a mês',True):
+        st.write(f'Aqui ficará o gráfico de vendas da {loja}!')
+        st.write('..................')
+
+
+
+
+elif authentication_status == False: 
+    st.sidebar.error('Nome de usuário/senha incorreto') 
+elif authentication_status == None: 
+    st.sidebar.warning('Por favor, digite seu nome de usuário e senha')
+
+
+
+st.write('Hello World!')
--- venv/Scripts/runxlrd.py
+++ venv/Scripts/runxlrd.py
@@ -1,4 +1,4 @@
-#!C:\Users\enogu\OneDrive - UDESC Universidade do Estado de Santa Catarina\Documentos\Emerson\dev\ShopfarmaAnalyser\venv\scripts\python.exe
+#!C:\Users\emers\OneDrive - UDESC Universidade do Estado de Santa Catarina\SHOPFARMA\ShopfarmaAnalyser\ShopfarmaAnalyser\venv\Scripts\python.exe
 # Copyright (c) 2005-2012 Stephen John Machin, Lingfo Pty Ltd
 # This script is part of the xlrd package, which is released under a
 # BSD-style licence.
