81df83e7cca75303a63a3a6dcc5bd0058a27de1f
emerson <emerson.soares.sc@gmail.com>
2022-03-26 22:02:58 -0300

    commit1

--- app.py
+++ app.py
@@ -2,6 +2,27 @@ import streamlit as st
 #import streamlit_authenticator as stauth
 import pandas as pd 
 from functions.ImportaRelatorios import VendasPorDatas
+from orc_lojas import HelloWorld
+
+st.write(HelloWorld())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #st.sidebar.image('images\logo_shopfarama.png')
@@ -17,14 +38,14 @@ from functions.ImportaRelatorios import VendasPorDatas
     
 # Menu Lateral
 #st.sidebar.write('Bem-vindo *%s*' % (name)) 
-with st.sidebar.expander('Análise de dados'):
-    st.write('Evolução do número de clientes')
-    st.write('Evolução do Ticket Médio')
-    st.write('Análise de filial')
-with st.sidebar.expander('Análise de dados'):
-    st.write('Evolução do número de clientes')
-    st.write('Evolução do Ticket Médio')
-    st.write('Análise de filial')
+#with st.sidebar.expander('Análise de dados'):
+#    st.write('Evolução do número de clientes')
+#    st.write('Evolução do Ticket Médio')
+#    st.write('Análise de filial')
+#with st.sidebar.expander('Análise de dados'):
+#    st.write('Evolução do número de clientes')
+#    st.write('Evolução do Ticket Médio')
+#    st.write('Análise de filial')
 
 #################################################################
 # Dashboard Principal
@@ -39,23 +60,23 @@ with st.sidebar.expander('Análise de dados'):
 # Gráfico de saúde financeira das lojas
 
 # Filtrar dados por loja
-loja = st.selectbox('Selecione uma loja:',('CENTRAL', 'MATRIZ', 'RIO VERMELHO', 'VARGEM', 'CENTRINHO', 'CALIL'))
-metricas = {
-    'venda_liquida':10
-}
+#loja = st.selectbox('Selecione uma loja:',('CENTRAL', 'MATRIZ', 'RIO VERMELHO', 'VARGEM', 'CENTRINHO', 'CALIL'))
+#metricas = {
+#    'venda_liquida':10
+#}
 # Lineup --- Venda Líquida - Entrada de Capital - Saída de Capital - Saldo de Capital
-met1, met2, met3, met4 = st.columns(4)
-met1.metric(label='Venda Líquida', value=metricas['venda_liquida'], delta='-0.6')
-met2.metric(label='Entrada Efetiva de Capital', value='92.4K', delta='-0.8')
-met3.metric(label='Saída Efetiva de Capital', value='-93.0K', delta='10.6')
-met4.metric(label='Saldo Efetivo de Capital', value='-0.6K', delta='-0.1')
+#met1, met2, met3, met4 = st.columns(4)
+#met1.metric(label='Venda Líquida', value=metricas['venda_liquida'], delta='-0.6')
+#met2.metric(label='Entrada Efetiva de Capital', value='92.4K', delta='-0.8')
+#met3.metric(label='Saída Efetiva de Capital', value='-93.0K', delta='10.6')
+#met4.metric(label='Saldo Efetivo de Capital', value='-0.6K', delta='-0.1')
 
 # Gráfico com evolução das vendas mês a mês selecionável por loja
-with st.expander('Evolução das vendas mês a mês',True):
-    st.write(f'Aqui ficará o gráfico de vendas da {loja}!')
-    st.write('..................')
+#with st.expander('Evolução das vendas mês a mês',True):
+#    st.write(f'Aqui ficará o gráfico de vendas da {loja}!')
+#    st.write('..................')
 
 
 
 
-st.write('Hello World!')
+#st.write('Hello World!')
---
+++ orc_lojas.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import streamlit as st 
+
+def HelloWorld():
+    hw = st.write('Hello World!')
+    return hw
