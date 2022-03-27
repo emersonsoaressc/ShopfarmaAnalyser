2b61e78c11ce2a63e8e5bcafd541377c28c471fb
emerson <emerson.soares.sc@gmail.com>
2022-03-26 21:46:02 -0300

    commit1

--- app.py
+++ app.py
@@ -1,67 +1,60 @@
 import streamlit as st 
-import streamlit_authenticator as stauth
+#import streamlit_authenticator as stauth
 import pandas as pd 
 from functions.ImportaRelatorios import VendasPorDatas
 
 
 st.sidebar.image('images\logo_shopfarama.png')
-names = ['Emerson Soares']
-usernames = ['emerson']
-passwords = ['123']
-hashed_passwords = stauth.Hasher(passwords).generate()
-authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
-    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
-name, authentication_status, username = authenticator.login('Login','sidebar')
+#names = ['Emerson Soares']
+#usernames = ['emerson']
+#passwords = ['123']
+#hashed_passwords = stauth.Hasher(passwords).generate()
+#authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
+#    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+#name, authentication_status, username = authenticator.login('Login','sidebar')
 
-if authentication_status: 
+#if authentication_status: 
     
-    # Menu Lateral
-    st.sidebar.write('Bem-vindo *%s*' % (name)) 
-    with st.sidebar.expander('Análise de dados'):
-        st.write('Evolução do número de clientes')
-        st.write('Evolução do Ticket Médio')
-        st.write('Análise de filial')
-    with st.sidebar.expander('Análise de dados'):
-        st.write('Evolução do número de clientes')
-        st.write('Evolução do Ticket Médio')
-        st.write('Análise de filial')
-    
-    #################################################################
-    # Dashboard Principal
-    #################################################################
+# Menu Lateral
+#st.sidebar.write('Bem-vindo *%s*' % (name)) 
+with st.sidebar.expander('Análise de dados'):
+    st.write('Evolução do número de clientes')
+    st.write('Evolução do Ticket Médio')
+    st.write('Análise de filial')
+with st.sidebar.expander('Análise de dados'):
+    st.write('Evolução do número de clientes')
+    st.write('Evolução do Ticket Médio')
+    st.write('Análise de filial')
 
-    # Gráfico com evolução das vendas mês a mês selecionável por loja
-    # Gráfico com evolução do numero de clientes mês a mês
-    # Gráfico de ticket médio de loja e de funcionários
-    # Gráfico de Laboratórios mais vendidos
-    # Gráfico de distribuidoras mais compradas
-    # Gráfico com participação do tipo de custos 
-    # Gráfico de saúde financeira das lojas
+#################################################################
+# Dashboard Principal
+#################################################################
 
-    # Filtrar dados por loja
-    loja = st.selectbox('Selecione uma loja:',('CENTRAL', 'MATRIZ', 'RIO VERMELHO', 'VARGEM', 'CENTRINHO', 'CALIL'))
-    metricas = {
-        'venda_liquida':10
-    }
-    # Lineup --- Venda Líquida - Entrada de Capital - Saída de Capital - Saldo de Capital
-    met1, met2, met3, met4 = st.columns(4)
-    met1.metric(label='Venda Líquida', value=metricas['venda_liquida'], delta='-0.6')
-    met2.metric(label='Entrada Efetiva de Capital', value='92.4K', delta='-0.8')
-    met3.metric(label='Saída Efetiva de Capital', value='-93.0K', delta='10.6')
-    met4.metric(label='Saldo Efetivo de Capital', value='-0.6K', delta='-0.1')
+# Gráfico com evolução das vendas mês a mês selecionável por loja
+# Gráfico com evolução do numero de clientes mês a mês
+# Gráfico de ticket médio de loja e de funcionários
+# Gráfico de Laboratórios mais vendidos
+# Gráfico de distribuidoras mais compradas
+# Gráfico com participação do tipo de custos 
+# Gráfico de saúde financeira das lojas
 
-    # Gráfico com evolução das vendas mês a mês selecionável por loja
-    with st.expander('Evolução das vendas mês a mês',True):
-        st.write(f'Aqui ficará o gráfico de vendas da {loja}!')
-        st.write('..................')
+# Filtrar dados por loja
+loja = st.selectbox('Selecione uma loja:',('CENTRAL', 'MATRIZ', 'RIO VERMELHO', 'VARGEM', 'CENTRINHO', 'CALIL'))
+metricas = {
+    'venda_liquida':10
+}
+# Lineup --- Venda Líquida - Entrada de Capital - Saída de Capital - Saldo de Capital
+met1, met2, met3, met4 = st.columns(4)
+met1.metric(label='Venda Líquida', value=metricas['venda_liquida'], delta='-0.6')
+met2.metric(label='Entrada Efetiva de Capital', value='92.4K', delta='-0.8')
+met3.metric(label='Saída Efetiva de Capital', value='-93.0K', delta='10.6')
+met4.metric(label='Saldo Efetivo de Capital', value='-0.6K', delta='-0.1')
 
+# Gráfico com evolução das vendas mês a mês selecionável por loja
+with st.expander('Evolução das vendas mês a mês',True):
+    st.write(f'Aqui ficará o gráfico de vendas da {loja}!')
+    st.write('..................')
 
-
-
-elif authentication_status == False: 
-    st.sidebar.error('Nome de usuário/senha incorreto') 
-elif authentication_status == None: 
-    st.sidebar.warning('Por favor, digite seu nome de usuário e senha')
 
 
 
