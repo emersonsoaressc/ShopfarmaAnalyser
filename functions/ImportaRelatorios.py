4ef4324cb2eabdff90fab11eb501ab160698bef2
emerson <emerson.soares.sc@gmail.com>
2022-04-10 17:23:39 -0300

    commit1

--- functions/ImportaRelatorios.py
+++ functions/ImportaRelatorios.py
@@ -1,8 +1,8 @@
 import pandas as pd  
-
+import openpyxl
 
 def VendasPorDatas():
-    df = pd.read_excel('dados/venda_mensal.xls', header=0, usecols='B,E,L,M,N,O', engine='openpyxl').set_index('DATA')
+    df = pd.read_excel('dados/venda_mensal.xls', header=0, usecols='B,E,L,M,N,O').set_index('DATA')
     df = df[:-3]
     df['TOTAL ACUMULADO'] = df['TOTAL VENDAS'].cumsum()
     return df 
